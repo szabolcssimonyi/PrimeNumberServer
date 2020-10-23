178407b5fb4262f410a185cdfd0cb49fcd48a28c
szabolcs simonyi <szabolcs.simonyi@hotmail.com>
2020-10-23 20:26:31 +0200

    test modosítás feature 01-ben 01

--- PrimeNumber.Api/Startup.cs
+++ PrimeNumber.Api/Startup.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 
 namespace PrimeNumber.Api
 {
+    // test modosítás feature 01-ben 01
     public class Startup
     {
         public static Autofac.Module AutofacModule;
